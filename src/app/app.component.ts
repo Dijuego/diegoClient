922b4d9e24dfc9c9783bf951c73b54a66a076865
Dijuego <diego.hernandez.735@my.csun.edu>
2025-02-07 18:18:05 -0800

    Initial Commit Comp 584

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
+import { WeatherForecast } from "./weather-forecast";
+import { environment } from "../environments/environment.development";
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.css',
+  imports: [
+    NavBarComponent,
+    RouterOutlet,
+    
+  ]
 })
-export class AppComponent {
-  title = 'diegoClient';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    
+  }
+  
+
+  title = 'dh2nd.client';
 }
