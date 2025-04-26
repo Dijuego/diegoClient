f4e5fa6b800461aa681af4acfe2ea302354348b5
Dijuego <diego.hernandez.735@my.csun.edu>
2025-04-25 18:15:20 -0700

    auth on client

--- src/app/auth/login.component.ts
+++ src/app/auth/login.component.ts
@@ -2,8 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, UntypedFormGroup, Validators } from '@angular/forms';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { LoginRequest } from './login-request';
+import { AuthService } from './auth.service';
 @Component({
   selector: 'app-login',
   imports: [
@@ -19,7 +20,7 @@ export class LoginComponent implements OnInit {
 
   form!: FormGroup;
 
-  constructor(){
+  constructor(private authService:AuthService, private router: Router){
 
   }
   ngOnInit(): void {
@@ -33,6 +34,14 @@ export class LoginComponent implements OnInit {
       userName: this.form.controls['userName'].value,
       password: this.form.controls['password'].value
     };
-    
+    this.authService.login(loginRequest).subscribe({
+      next: (result) => {
+        console.log(result);
+        if(result.success){
+          this.router.navigate(["/"]);
+        }
+      },
+      error: error => console.error(error)
+    })
   }
 }
